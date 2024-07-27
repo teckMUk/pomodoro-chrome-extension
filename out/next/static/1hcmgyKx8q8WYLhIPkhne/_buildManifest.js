11173ff514bcd922fb45f3693a2f8f5c4081392a
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-07-27 23:48:24 +0300

    fix pause button action and improve code

---
+++ out/next/static/1hcmgyKx8q8WYLhIPkhne/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/eb117715b3a14e11.css","static/chunks/pages/index-b841fcf1cf156315.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
